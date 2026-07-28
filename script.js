8fe782e6cd5755d936349aaa6c000d16cad0034f
shivaador-prog <shivaador@gmail.com>
2026-07-28 19:33:16 +0330

    Update script.js

--- script.js
+++ script.js
@@ -212,9 +212,9 @@ function showHero(index){
    if (window.innerWidth <= 768) {
 
     if(stage==9){
-        hero.style.width = "600px";
+        hero.style.width = "500px";
         hero.style.left = "22%";
-        hero.style.top = "-8%";
+        hero.style.top = "-20%";
     }else{
         hero.style.width = "120px";
         hero.style.left = "22%";
